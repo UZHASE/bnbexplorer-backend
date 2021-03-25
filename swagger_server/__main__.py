d8582dd7799e727a65b92e3be23c0f79daca9840
Julius Willems <julius.willems@staff.cloud>
2021-03-25 19:51:45 +0100

    Added heroku config

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import connexion
-
+import os
 from swagger_server import encoder
 from flask_cors import CORS
 
@@ -11,7 +11,7 @@ def main():
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Swagger Petstore'})
     CORS(app.app)
-    app.run(port=8080)
+    app.run(port=int(os.environ.get('PORT', 8080)))
 
 
 if __name__ == '__main__':
