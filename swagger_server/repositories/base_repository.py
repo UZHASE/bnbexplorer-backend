39af4f2d333ba95d39aa3e5cf80336eb3deb6c02
Julius Willems <julius.willems@staff.cloud>
2021-04-02 08:44:06 +0200

    bnbexplorer-backend-11 Manage env variables with .env file

--- swagger_server/repositories/base_repository.py
+++ swagger_server/repositories/base_repository.py
@@ -5,7 +5,7 @@ import sqlite3
 class Repository(object):
 
     def __init__(self):
-        self.__connection = sqlite3.connect('database/bnbexplorer.sqlite')
+        self.__connection = sqlite3.connect(str(os.environ.get('DB_PATH', 'database/bnbexplorer.sqlite')))
         # TODO: Should we keep this? Allows to get query results as dicts.
         # See https://stackoverflow.com/questions/3300464/how-can-i-get-dict-from-sqlite-query
         self.__connection.row_factory = sqlite3.Row
