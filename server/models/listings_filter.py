d0b6e645644d5f77efaa85e6c8ae1ae57a5d2622
Cristian De Iaco <cristian.deiaco@uzh.ch>
2021-04-06 20:02:53 +0200

    POC for query builder implementing the findAllListings method

---
+++ server/models/listings_filter.py
@@ -0,0 +1,39 @@
+class ListingsFilter:
+    def __init__(self, listing_name, host_id, host_name, neighbourhood, area, price_min,
+                 price_max, min_nights, availability, listings_per_host, room_type):
+        """Listing - a model defined in Swagger
+
+                :param listing_name: Filter by Listing name
+                :type listing_name: str
+                :param host_id: Filter by Host ID
+                :type host_id: int
+                :param host_name: Filter by Host name
+                :type host_name: str
+                :param neighbourhood:  Filter by neighbourhood of Listing
+                :type neighbourhood: str
+                :param area: Filter by area of Listing
+                :type area: str
+                :param price_min: Filter by min price of Listings
+                :type price_min: int
+                :param price_max: Filter by max price of Listings
+                :type price_max: int
+                :param min_nights: Filter by min nights for Listings
+                :type min_nights: int
+                :param availability: Filter by availability of Listings
+                :type availability: int
+                :param listings_per_host: Filter by number of Listings per Host
+                :type listings_per_host: int
+                :param room_type: Filter by room type of Listings
+                :type room_type: str
+                """
+        self.listing_name = listing_name
+        self.host_id = host_id
+        self.host_name = host_name
+        self.neighbourhood = neighbourhood
+        self.area = area
+        self.price_min = price_min
+        self.price_max = price_max
+        self.min_nights = min_nights
+        self.availability = availability
+        self.listings_per_host = listings_per_host
+        self.room_type = room_type
--- server/repositories/listing_repsotory.py
+++ server/repositories/listing_repsotory.py
@@ -1,3 +1,5 @@
+from pypika import Query, Table, Schema
+
 from server.repositories.base_repository import Repository
 from server.models.listing import Listing as Listing_Model
 from server.models.host import Host as Host_Model
@@ -5,16 +7,13 @@ from server.models.host import Host as Host_Model
 
 class Listing(Repository):
 
-    def get_all(self):
-        query_results = self._db.execute('''
-            SELECT 
-                listings.id, listings.name, neighbourhood, area, latitude, longitude, room_type roomType, price, 
-                min_nights minNights, num_of_reviews numOfReviews, availability, hosts.id host_id, hosts.name host_name, 
-                hosts.num_of_listings
-            FROM listings, hosts
-            LIMIT 1000
-        ''').fetchall()
+    def get_all(self, listings_filter):
 
+        # build query with provided params
+        query = self.build_query(listings_filter)
+        # execute query
+        query_results = self._db.execute(query).fetchall()
+        # object mapping
         result = []
         for row in query_results:
             row = dict(row)
@@ -27,3 +26,51 @@ class Listing(Repository):
             result.append(listing)
 
         return result
+
+    def build_query(self, filter):
+        # tables
+        listings = Table('listings')
+        hosts = Table('hosts')
+        # query building
+        query = Query \
+            .from_(listings) \
+            .from_(hosts) \
+            .select(
+                listings.id, listings.name, listings.neighbourhood, listings.area, listings.longitude, listings.latitude,
+                listings.room_type.as_('roomType'), listings.price, listings.min_nights.as_('minNights'),
+                listings.num_of_reviews.as_('numOfReviews'), listings.availability, hosts.id.as_('host_id'),
+                hosts.name.as_('host_name'), hosts.num_of_listings)
+        # WHERE clauses
+        if filter.listing_name is not None:
+            # TODO: Does not need to be exact match, we could SQL's LIKE function for partial matching
+            query = query.where(listings.name == filter.listing_name)
+        if filter.host_id is not None:
+            query = query.where(hosts.id == filter.host_id)
+        if filter.host_name is not None:
+            # TODO: Does not need to be exact match, we could SQL's LIKE function for partial matching
+            query = query.where(hosts.name == filter.host_name)
+        if filter.neighbourhood is not None:
+            # TODO: We can accept a list and use SQL's IN function to filter for the given values
+            query = query.where(listings.neighbourhood == filter.neighbourhood)
+        if filter.area is not None:
+            # TODO: We can accept a list and use SQL's IN function to filter for the given values
+            query = query.where(listings.area == filter.area)
+        if filter.price_min is not None:
+            query = query.where(listings.price >= filter.price_min)
+        if filter.price_max is not None:
+            query = query.where(listings.price <= filter.price_max)
+        if filter.min_nights is not None:
+            query = query.where(listings.min_nights >= filter.min_nights)
+        if filter.availability is not None:
+            # TODO: Should this input be handled as an enum, or min/max values? (currently: min value)
+            query = query.where(listings.availability >= filter.availability)
+        if filter.listings_per_host is not None:
+            # TODO: Should this input be handled as an enum, or min/max values? (currently: min value)
+            query = query.where(hosts.num_of_listings >= filter.listings_per_host)
+        if filter.room_type is not None:
+            # TODO: We can accept a list and use SQL's IN function to filter for the given values
+            query = query.where(listings.room_type == filter.room_type)
+        # LIMIT clause
+        query = query.limit(self.ROW_LIMIT)
+
+        return str(query)
