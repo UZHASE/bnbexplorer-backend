f6166603f2921cd5cdfba45ae72005544f965ae5
Cristian De Iaco <cristian.deiaco@uzh.ch>
2021-04-07 12:37:33 +0200

    add tests for listing repo

--- server/models/listings_filter.py
+++ server/models/listings_filter.py
@@ -1,31 +1,32 @@
 class ListingsFilter:
-    def __init__(self, listing_name, host_id, host_name, neighbourhood, area, price_min,
-                 price_max, min_nights, availability, listings_per_host, room_type):
-        """Listing - a model defined in Swagger
+    def __init__(self, listing_name=None, host_id=None, host_name=None,
+                 neighbourhood=None, area=None, price_min=None, price_max=None,
+                 min_nights=None, availability=None, listings_per_host=None, room_type=None):
+        """ListingsFilter - filter options to query Listings
 
-                :param listing_name: Filter by Listing name
-                :type listing_name: str
-                :param host_id: Filter by Host ID
-                :type host_id: int
-                :param host_name: Filter by Host name
-                :type host_name: str
-                :param neighbourhood:  Filter by neighbourhood of Listing
-                :type neighbourhood: str
-                :param area: Filter by area of Listing
-                :type area: str
-                :param price_min: Filter by min price of Listings
-                :type price_min: int
-                :param price_max: Filter by max price of Listings
-                :type price_max: int
-                :param min_nights: Filter by min nights for Listings
-                :type min_nights: int
-                :param availability: Filter by availability of Listings
-                :type availability: int
-                :param listings_per_host: Filter by number of Listings per Host
-                :type listings_per_host: int
-                :param room_type: Filter by room type of Listings
-                :type room_type: str
-                """
+        :param listing_name: Filter by Listing name
+        :type listing_name: str
+        :param host_id: Filter by Host ID
+        :type host_id: int
+        :param host_name: Filter by Host name
+        :type host_name: str
+        :param neighbourhood:  Filter by neighbourhood of Listing
+        :type neighbourhood: str
+        :param area: Filter by area of Listing
+        :type area: str
+        :param price_min: Filter by min price of Listings
+        :type price_min: int
+        :param price_max: Filter by max price of Listings
+        :type price_max: int
+        :param min_nights: Filter by min nights for Listings
+        :type min_nights: int
+        :param availability: Filter by availability of Listings
+        :type availability: int
+        :param listings_per_host: Filter by number of Listings per Host
+        :type listings_per_host: int
+        :param room_type: Filter by room type of Listings
+        :type room_type: str
+         """
         self.listing_name = listing_name
         self.host_id = host_id
         self.host_name = host_name
--- server/repositories/listing_repsotory.py
+++ server/repositories/listing_repsotory.py
@@ -1,16 +1,20 @@
-from pypika import Query, Table, Schema
+import logging
+
+from pypika import Query, Table
+from pypika.functions import Lower
 
 from server.repositories.base_repository import Repository
 from server.models.listing import Listing as Listing_Model
 from server.models.host import Host as Host_Model
 
+log = logging.getLogger('listing_repo')
+
 
 class Listing(Repository):
 
     def get_all(self, listings_filter):
-
         # build query with provided params
-        query = self.build_query(listings_filter)
+        query = self.build_get_all_query(listings_filter)
         # execute query
         query_results = self._db.execute(query).fetchall()
         # object mapping
@@ -27,14 +31,15 @@ class Listing(Repository):
 
         return result
 
-    def build_query(self, filter):
+    def build_get_all_query(self, filter):
         # tables
         listings = Table('listings')
         hosts = Table('hosts')
         # query building
         query = Query \
             .from_(listings) \
-            .from_(hosts) \
+            .join(hosts)\
+            .on(listings.host_id == hosts.id)\
             .select(
                 listings.id, listings.name, listings.neighbourhood, listings.area, listings.longitude, listings.latitude,
                 listings.room_type.as_('roomType'), listings.price, listings.min_nights.as_('minNights'),
@@ -43,12 +48,12 @@ class Listing(Repository):
         # WHERE clauses
         if filter.listing_name is not None:
             # TODO: Does not need to be exact match, we could SQL's LIKE function for partial matching
-            query = query.where(listings.name == filter.listing_name)
+            query = query.where(Lower(listings.name).like(Lower(filter.listing_name + '%')))
         if filter.host_id is not None:
             query = query.where(hosts.id == filter.host_id)
         if filter.host_name is not None:
             # TODO: Does not need to be exact match, we could SQL's LIKE function for partial matching
-            query = query.where(hosts.name == filter.host_name)
+            query = query.where(Lower(hosts.name).like(Lower(filter.host_name + '%')))
         if filter.neighbourhood is not None:
             # TODO: We can accept a list and use SQL's IN function to filter for the given values
             query = query.where(listings.neighbourhood == filter.neighbourhood)
@@ -69,8 +74,9 @@ class Listing(Repository):
             query = query.where(hosts.num_of_listings >= filter.listings_per_host)
         if filter.room_type is not None:
             # TODO: We can accept a list and use SQL's IN function to filter for the given values
-            query = query.where(listings.room_type == filter.room_type)
+            query = query.where(Lower(listings.room_type) == Lower(filter.room_type))
         # LIMIT clause
         query = query.limit(self.ROW_LIMIT)
 
-        return str(query)
+        log.debug(query.get_sql())
+        return query.get_sql()
