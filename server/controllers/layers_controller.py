f2c3a728901429f24860d8d5afc800e798034169
Cristian De Iaco <cristian.deiaco@uzh.ch>
2021-04-10 20:06:14 +0200

    layer controller implementation

--- server/controllers/layers_controller.py
+++ server/controllers/layers_controller.py
@@ -1,14 +1,17 @@
 import connexion
 import six
+from flask import jsonify
 
-from server.models.layer import Layer  # noqa: E501
+from server.models.layer import Layer
+from server.repositories.layer_repository import Layer as Layer_Repository
 from server import util
+from server.repositories.layer_strategy import CrimeLayerStrategy, HealthLayerStrategy
 
 
-def find_complaints_layer():  # noqa: E501
+def find_complaints_layer():
     """Retrieve Complaints Layer details
 
-    Returns details to diyplay Complaints Layer (amount per neighbourhood) # noqa: E501
+    Returns details to diyplay Complaints Layer (amount per neighbourhood)
 
 
     :rtype: Layer
@@ -16,23 +19,23 @@ def find_complaints_layer():  # noqa: E501
     return 'do some magic!'
 
 
-def find_crime_layer():  # noqa: E501
+def find_crime_layer():
     """Retrieve Crime Layer details
 
-    Returns details to diyplay Crime Layer (amount per neighbourhood) # noqa: E501
+    Returns details to diyplay Crime Layer (amount per neighbourhood)
 
 
     :rtype: Layer
     """
-    return 'do some magic!'
+    return jsonify(Layer_Repository(CrimeLayerStrategy()).get_all())
 
 
-def find_health_layer():  # noqa: E501
+def find_health_layer():
     """Retrieve Health Layer details
 
-    Returns details to diyplay Health Layer (amount per neighbourhood) # noqa: E501
+    Returns details to diyplay Health Layer (amount per neighbourhood)
 
 
     :rtype: Layer
     """
-    return 'do some magic!'
+    return jsonify(Layer_Repository(HealthLayerStrategy()).get_all())
