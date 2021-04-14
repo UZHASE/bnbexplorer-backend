e8860e189566d13090c3a4193bc80bb4b54c01fd
Julius Willems <julius.willems@staff.cloud>
2021-04-14 08:40:18 +0200

    bnbexplorer-backend-27 implemented endpoint and query for hosts/{id}

--- server/repositories/host_repository.py
+++ server/repositories/host_repository.py
@@ -1,18 +1,23 @@
 from server.repositories.base_repository import Repository
 from server.models.host import Host as Host_Model
+from pypika import Query, Table
 
 
 class Host(Repository):
 
     def get_by_id(self, host_id):
-        query_result = self._db.execute('''
-            SELECT id, name, num_of_listings numOfListings 
-            FROM hosts 
-            WHERE id=?
-        ''', (host_id,)).fetchone()
+        query = self.build_get_by_id_query(host_id)
 
-        return Host_Model.from_dict(dict(query_result))
+        return self.execute_select_query(query)
+
+    def build_get_by_id_query(self, host_id):
+        hosts = Table('hosts')
+
+        return Query\
+            .from_(hosts)\
+            .select(hosts.id, hosts.name, hosts.num_of_listings.as_('numOfListings'))\
+            .where(hosts.id == host_id)\
+            .get_sql()
 
     def map_result(self, query_results):
-        #TODO: Mapping
-        pass
+        return [Host_Model.from_dict(dict(row)) for row in query_results]
