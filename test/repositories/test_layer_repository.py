08b9e06b8cf8e0a2be870a483e9e7e4969e580ea
Julius Willems <julius.willems@staff.cloud>
2021-04-14 09:53:25 +0200

    bnbexplorer-backend-28 Implemented tests for complaint layer

--- test/repositories/test_layer_repository.py
+++ test/repositories/test_layer_repository.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from server.repositories.layer_repository import Layer
-from server.repositories.layer_strategy import HealthLayerStrategy, CrimeLayerStrategy
+from server.repositories.layer_strategy import HealthLayerStrategy, CrimeLayerStrategy, ComplaintLayerStrategy
 
 
 class TestLayer(TestCase):
@@ -15,3 +15,8 @@ class TestLayer(TestCase):
         crime_layer = Layer(CrimeLayerStrategy()).get_all()
         self.assertEqual('CRIME_LAYER', crime_layer.type)
         self.assertTrue(crime_layer.entries)
+
+    def test_complaint_layer(self):
+        complaint_layer = Layer(ComplaintLayerStrategy()).get_all()
+        self.assertEqual('COMPLAINT_LAYER', complaint_layer.type)
+        self.assertEqual(554, len(complaint_layer.entries))
