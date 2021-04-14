08959c62b4208f05e1cc6e8ba2cb13bcc3cbeb19
Julius Willems <julius.willems@staff.cloud>
2021-04-14 10:20:30 +0200

    bnbexplorer-backend-34 Fixed bool cast

--- server/__main__.py
+++ server/__main__.py
@@ -6,6 +6,7 @@ from server import encoder
 from flask_cors import CORS
 import sqlite3
 from dotenv import load_dotenv
+from distutils import util
 
 load_dotenv()
 env = os.environ
@@ -15,8 +16,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'NYC AirBnB Explorer'}, pythonic_params=True)
-    CORS(app.app)
-    app.run(port=int(env.get('PORT', 8080)), debug=bool(env.get('DEBUG', False)))
+    app.run(port=int(env.get('PORT', 8080)), debug=bool(util.strtobool(env.get('DEBUG', 'False'))))
 
 
 if __name__ == '__main__':
